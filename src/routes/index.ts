7a35072fcf16ec3955cea813f04d95f072fbd79f
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-03-23 21:18:17 -0300

    User classes complete

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
+import { authenticateRoutes } from './authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
+import { usersRoutes } from './user.routes';
 
 const routes = Router();
 
 routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationsRoutes);
+routes.use('/users', usersRoutes);
+routes.use(authenticateRoutes);
 
 export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import swaggerUI from 'swagger-ui-express';
 
+import { AppError } from './errors/AppError';
 import { routes } from './routes';
 import swaggerFile from './swagger.json';
 
@@ -16,6 +18,20 @@ app.use(express.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message} `,
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('🖥 Server started on port 3333!');
