4d2b5b0d578cc2a12aab791ce51d7382a63dad07
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-03-29 15:55:55 -0300

    test: ensure that no be able to create a category with the same name

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,5 +1,5 @@
+import { AppError } from '../../../../errors/AppError';
 import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
-import { CreateCategoryController } from './CreateCategoryController';
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
 let createCategoryUseCase: CreateCategoryUseCase;
@@ -25,5 +25,22 @@ describe('Create Category', () => {
       category.name,
     );
     expect(categoryCreated).toHaveProperty('id');
+  });
+
+  test('Should not to be able to create a new category with the same name', async () => {
+    await expect(async () => {
+      const category = {
+        name: 'any_valid_name',
+        description: 'any_valid_description',
+      };
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
