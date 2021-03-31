8f438df288049900aec035935006d0b64f87cf3a
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-03-31 12:37:40 -0300

    feat: implements a car routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 import { authenticateRoutes } from './authenticate.routes';
+import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './user.routes';
 
 const routes = Router();
 
+routes.use('/cars', carsRoutes);
 routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationsRoutes);
 routes.use('/users', usersRoutes);
