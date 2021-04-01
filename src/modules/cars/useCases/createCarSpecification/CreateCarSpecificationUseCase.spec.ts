5622f65b0e385c60c81fbde519c9b2b40a406dc7
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-01 15:50:39 -0300

    test: ensure to add specifications a car add not allow add a specification a non-existent car

---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -0,0 +1,46 @@
+import { CarRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
+
+import { CreateCarSpecificationUseCase } from './CreateCarSpecificationUseCase';
+
+let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
+let carsRepositoryInMemory: CarRepositoryInMemory;
+
+describe('Create Car Specification', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarRepositoryInMemory();
+    createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
+      carsRepositoryInMemory,
+    );
+  });
+
+  test('Should not be able to add a new specification to a non-existent car', async () => {
+    expect(async () => {
+      const car_id = '1234';
+      const specifications_id = ['54321'];
+      await createCarSpecificationUseCase.execute({
+        car_id,
+        specifications_id,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  test('Should be able to add a new specification to a car', async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: 'any_name',
+      description: 'any_description',
+      daily_rate: 100,
+      license_plate: 'any_plate',
+      fine_amount: 60,
+      brand: 'any_brand',
+      category_id: 'valid_category_id',
+    });
+
+    const specifications_id = ['54321'];
+
+    await createCarSpecificationUseCase.execute({
+      car_id: car.id,
+      specifications_id,
+    });
+  });
+});
