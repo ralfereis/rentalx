1fa4b5301c4d03ab098f2957cb131e42fc970f68
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-01 11:38:42 -0300

    feat: create a listing car by name, brand and category_id

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -25,12 +25,32 @@ class CarRepositoryInMemory implements ICarsRepository {
       brand,
       category_id,
     });
+
     this.cars.push(car);
 
     return car;
   }
   async findByLicensePlate(license_plate: string): Promise<Car> {
     return this.cars.find(car => car.license_plate === license_plate);
+  }
+  async findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string,
+  ): Promise<Car[]> {
+    const carsAvailable = this.cars.filter(car => {
+      if (
+        car.available === true ||
+        (brand && car.brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
+      ) {
+        return car;
+      }
+      return null;
+    });
+
+    return carsAvailable;
   }
 }
 export { CarRepositoryInMemory };
