61babdf589b5e965f4f9d0e45fcc9c77335ec2c1
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-05 22:04:52 -0300

    chore: configure a database to integration test

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'localhost'): Promise<Connection> => {
+export default async (host = 'database'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOptions, {
-      // process.env.NODE_ENV === 'test' ? 'localhost' : host
-      host,
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database:
         process.env.NODE_ENV === 'test'
           ? 'rentalx_test'
