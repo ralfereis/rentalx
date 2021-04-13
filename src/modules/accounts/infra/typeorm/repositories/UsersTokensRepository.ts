fdf71dc7f80c5b5006a1a469f9abc730e74c72ce
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-04-13 20:26:54 -0300

    feat: implements RefreshTokenUseCase

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -23,5 +23,18 @@ class UsersTokensRepository implements IUsersTokensRepository {
     await this.repository.save(userToken);
     return userToken;
   }
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string,
+  ): Promise<UserTokens> {
+    const usersTokens = await this.repository.findOne({
+      user_id,
+      refresh_token,
+    });
+    return usersTokens;
+  }
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
 }
 export { UsersTokensRepository };
